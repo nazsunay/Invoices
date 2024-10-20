8025b1b587e6ee62de4efddbbace612e6589f7a0
Nazlı Sunay Bulbul <nazlisunaybulbul@gmail.com>
2024-10-20 11:34:57 +0300

    Program.cs Düzenlendi

--- Program.cs
+++ Program.cs
@@ -24,11 +24,11 @@ namespace Invoices
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+            {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            //}
+            }
 
             app.UseHttpsRedirection();
 
